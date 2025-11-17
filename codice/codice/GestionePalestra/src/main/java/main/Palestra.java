52db0db361632d8073755270751ccf591d52e5df
CoachCasira <m.casiraghi9@studenti.unibg.it>
2025-11-17 22:16:57 +0100

    MVC e log4j2
    Strutturato codice con architettura MVC come da documentazione. Aggiunta l'ultima versione di log4j, aggiunto file log4j2.xml per testarne il suo funzionamento

---
+++ codice/codice/GestionePalestra/src/main/java/main/Palestra.java
@@ -0,0 +1,13 @@
+package main;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+public class Palestra {
+	
+	 private static final Logger logger = LogManager.getLogger(Palestra.class);
+	public static void main(String[] args) {
+		logger.info("Avvio dell'applicazione GestionePalestra");
+		logger.debug("Inizializzazione completata (debug di esempio)");
+
+	}
+
+}
